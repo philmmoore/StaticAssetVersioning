02d8be703579c27e83d9b13bf4845a8a718bb2bf
Philip Moore <phil@mooreish.co.uk>
2013-04-04 17:02:49 +0100

    Finished example file

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?= StaticAsset::version('testfile.php'); ?>
+<?php 
+
+	/**
+	 * Usage
+	 * Version at will!
+	 */
+
+	include 'class.static_asset.php';
+	echo StaticAsset::version('testfile.php'); 
+
+?>
